4179ff83bedd80c7e40ba0b8f9d130ce6a5f5061
FlowTech-Digital <admin@flowtechai.com.au>
2025-12-12 17:05:58 +1100

    Delete src/app/api/portal/invoices/[token]/pay/route.ts

--- src/app/api/portal/invoices/[token]/pay/route.ts
+++
@@ -1,5 +0,0 @@
-// This route has been moved to /api/portal/invoice-pay/[token]/route.ts
-// This empty file prevents the nested route validation error in Next.js 15
-// See: https://github.com/vercel/next.js/issues/69692
-
-export {}
